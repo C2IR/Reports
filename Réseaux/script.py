d1d31b6d5d76a4b7800d9e625c9938d0b0d502e6
mhdgning131 <gningmoustapha078@gmail.com>
2026-02-06 04:01:48 +0000

    Third !

--- Réseaux/script.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Go in each folder and create a file named "Readme.md" with the content "This is the README file for [folder name]"
-for folder in os.listdir():
-    if os.path.isdir(folder):
-        with open(os.path.join(folder, "Readme.md"), "w") as f:
-            f.write(f"This is the README file for {folder}")
