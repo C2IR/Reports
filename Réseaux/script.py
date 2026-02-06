6f0a4951095032cd58e511aa5ecf1f95b13be176
mhdgning131 <gningmoustapha078@gmail.com>
2026-02-06 03:58:18 +0000

    Added README for each group so github wont ignore them !

--- Réseaux/script.py
+++ Réseaux/script.py
@@ -1,4 +1,7 @@
 import os
 
-for i in range (1, 8):
-    os.system(f"mkdir Groupe_{i}")
+# Go in each folder and create a file named "Readme.md" with the content "This is the README file for [folder name]"
+for folder in os.listdir():
+    if os.path.isdir(folder):
+        with open(os.path.join(folder, "Readme.md"), "w") as f:
+            f.write(f"This is the README file for {folder}")
